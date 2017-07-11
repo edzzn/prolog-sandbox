728f3a5eb1c99ec3f10773a7d795c7c5d474574a
Edisson Reinozo <edisson350@gmail.com>
2017-07-11 12:52:34 -0500

    Familia Completed

--- familia.pl
+++ familia.pl
@@ -47,7 +47,8 @@ esposos(peter, jan).
 
  /* X es abuelo de Y - No distingue genero*/
 abuelo(X, Y) :- padres(J, K, Y),
-    			( padres(_, X,J); padres(_, X,K)).
+    			( padres(_, X, J); padres(_, X,K);
+    			 padres(X, _, J); padres(X, _, K)).
 
  /* X es padre de Y */
 padre(X, Y) :- padres(_, X, Y), hombre(X).
@@ -116,86 +117,4 @@ primo(X, Y) :- padres(Padre1, Madre1, X),
                ).    
     
 /* X es soltero - No distingue el genero*/
-soltero(X) :- not(esposos(_, X)).hombre(frank).
-hombre(stanley).
-hombre(colin).
-hombre(david).
-hombre(shane).
-hombre(charlie).
-hombre(tom).
-hombre(chris).
-hombre(peter).
-hombre(todd).
-
-mujer(joyce).
-mujer(pauline).
-mujer(karen).
-mujer(janice).
-mujer(jan).
-mujer(lauren).
-mujer(hayley).
-mujer(katie).
-mujer(amy-leigh).
-
-padres(joyce, frank, david).
-padres(joyce, frank, janice).
-padres(joyce, frank, colin).
-
-padres(pauline, stanley, chris).
-padres(pauline, stanley, jan).
-
-padres(karen, colin, shane).
-padres(karen, colin, louren).
-
-padres(janice, chris, hayley).
-padres(janice, chris, katie).
-padres(janice, chris, charlie).
-padres(janice, chris, tom).
-
-padres(jan, peter, todd).
-padres(jan, peter, amy-leigh).
-
-esposos(joyce, frank).
-esposos(pauline, stanley).
-esposos(karen, colin).
-esposos(janice, chris).
-esposos(jan, peter).
-
- /* X es abuelo de Y */
-abuelo(X, Y) :- padres(J, K, Y),
-    			( padres(_, X,J); padres(_, X,K)).
-
- /* X es padre de Y */
-padre(X, Y) :- padres(_, X, Y), hombre(X).
-
- /* X es padre de Y */
-madre(X, Y) :- padres(_, X, Y), mujer(X).
-
-/* X es hermano de Y */
-hermano(X, Y) :- padres(Padre, Madre, X),
-    			 padres(Padre, Madre, Y),
-    			 hombre(X).
-
-/* X es hermana de Y */
-hermana(X, Y) :- padres(Padre, Madre, X),
-    			 padres(Padre, Madre, Y),
-    			 mujer(X).
-
-/* X es tio de Y */
-tio(X, Y) :- padres(Padre, Madre, Y),
-    		(hermano(X, Padre); hermano(X, Madre)).
-
-/* X es tia de Y */
-tia(X, Y) :- padres(Padre, Madre, Y),
-    		(hermana(X, Padre); hermana(X, Madre)).
-
-/* X es suegro de Y */
-suegro(X, Y) :- esposos(X, Pareja),
-    			padre(Y, Pareja).
-
-    
-
-/* X es soltero */
 soltero(X) :- not(esposos(_, X)).
-
-
